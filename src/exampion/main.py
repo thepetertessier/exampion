a4badb24172b710507af5031ee4c426f33e8e697
Peter Tessier <fpv5gr@virginia.edu>
2026-05-07 02:19:37 +0000

    log part of token to debug

--- src/exampion/main.py
+++ src/exampion/main.py
@@ -31,8 +31,14 @@ async def on_message(message: Message):
         await client.close()
 
 
-if __name__ == "__main__":
+def main():
     logger.add("logs/exampion.log", rotation="1 day", retention="30 days", level="DEBUG")
     logger.info("Started Exampion.")
-    client.run(get_cfg().BOT_TOKEN.get_secret_value())
+    token = get_cfg().BOT_TOKEN.get_secret_value()
+    logger.debug(f"Using bot token: {token[:8]}...")
+    client.run(token)
     logger.info("Exampion finished.")
+
+
+if __name__ == "__main__":
+    main()
